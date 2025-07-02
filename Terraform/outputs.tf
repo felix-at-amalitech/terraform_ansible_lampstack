859ef86f5e751c4e1f8140093812621c9db42d8c
Felix Frimpong <frimpong.felix@yahoo.com>
2025-07-02 16:32:39 +0000

    update --> added ssh private key

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,9 +1,15 @@
 output "web_public_ip" {
   value = module.web_tier.web_public_ip
 }
+
 output "app_private_ip" {
   value = module.app_tier.app_private_ip
 }
+
 output "db_endpoint" {
   value = module.db_tier.db_endpoint
+}
+
+output "ssh_key_name" {
+  value = aws_key_pair.lamp_key.key_name
 }
