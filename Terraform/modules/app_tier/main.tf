3e0deba69895e46d3edc28e3dac05cb9e522f002
Felix Frimpong <frimpong.felix@yahoo.com>
2025-07-02 16:24:42 +0000

    update --> added tag to app instance for easy identification in management console

--- Terraform/modules/app_tier/main.tf
+++ Terraform/modules/app_tier/main.tf
@@ -1,34 +1,52 @@
 resource "aws_security_group" "app_sg" {
   vpc_id = var.vpc_id
   ingress {
-    from_port = 9000
-    to_port = 9000
-    protocol = "tcp"
+    from_port       = 9000
+    to_port         = 9000
+    protocol        = "tcp"
     security_groups = [var.web_security_group_id]
   }
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "LAMP-App-SG"
+    Tier = "App"
   }
 }
 
 resource "aws_instance" "app" {
-  ami = "ami-000ec6c25978d5999"
-  instance_type = "t2.micro"
-  subnet_id = var.private_subnet_id
-  security_groups = [aws_security_group.app_sg.id]
-  key_name = var.key_name
+  ami             = "ami-0803576f0c0169402"
+  instance_type   = "t2.micro"
+  subnet_id       = var.private_subnet_id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+  key_name         = var.key_name
+  
+  tags = {
+    Name = "LAMP-App-Server"
+    Tier = "App"
+  }
+  
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y php
+              cat << 'PHP' > /var/www/html/app.php
+              <?php
+              function processRequest(\$data) {
+                  return "Processed: " . \$data;
+              }
+              ?>
+              PHP
               EOF
 }
--- Terraform/modules/app_tier/variables.tf
+++ Terraform/modules/app_tier/variables.tf
@@ -1,12 +1,19 @@
-variable "vpc_id" {}
-variable "private_subnet_id" {}
-variable "key_name" {}
-variable "web_security_group_id" {}
-
-# modules/app_tier/outputs.tf
-output "app_private_ip" {
-  value = aws_instance.app.private_ip
+variable "vpc_id" {
+  type = string
 }
-output "app_sg_id" {
-  value = aws_security_group.app_sg.id
+
+variable "private_subnet_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "web_security_group_id" {
+  type = string
+}
+
+variable "db_endpoint" {
+  type = string
 }
